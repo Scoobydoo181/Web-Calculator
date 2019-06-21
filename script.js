50042dd2d282be5d38b40777e7666d915d4c8028
Christopher Wilson <clwilson181@gmail.com>
2019-06-21 12:46:45 -0400

    Styled calculator and added animations to buttons

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+document.addEventListener("keydown", keyEventHandler(event));
+
 function setCalcText(symbol) {
   
     if(document.getElementById("output").innerHTML == 0.0) {
@@ -18,4 +20,123 @@ function clearCalcText() {
 
 function evaluateCalcText() {
     document.getElementById("output").innerHTML = eval(document.getElementById("output").innerHTML) ;
+}
+
+function keyEventHandler(event) {
+    
+    console.log(event.key);
+    setCalcText(event.key)
+    switch(event.key) {
+        case '(':
+            setCalcText('(');
+            break;
+
+        case ')':
+            setCalcText(')');
+            break;
+
+        case 'C':
+            clearCalcText();
+            break;
+        
+        case '7':
+            setCalcText('7');
+            break;
+
+        case '8':
+            setCalcText('8');
+            break;
+
+        case '9':
+            setCalcText('9');
+            break;    
+
+
+        case '4':
+            setCalcText('4');
+            break;
+
+        case '5':
+            setCalcText('5');
+            break;
+
+        case '6':
+            setCalcText('6');
+            break;
+        
+        case '1':
+            setCalcText('1');
+            break;
+
+        case '2':
+            setCalcText('2');
+            break;
+
+        case '3':
+            setCalcText('3');
+            break;   
+
+        case '/':
+            setCalcText('/');
+            break;
+
+        case '*':
+            setCalcText('*');
+            break;
+
+        case '-':
+            setCalcText('-');
+            break;
+        
+        case '+':
+            setCalcText('+');
+            break;
+
+        case '.':
+            setCalcText('.');
+            break;
+
+        case '0':
+            setCalcText('0');
+            break;    
+
+
+        case '=':
+            evaluateCalcText();
+            break;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
 }
